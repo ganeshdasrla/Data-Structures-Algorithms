55ae51f8af891c1f9bfc443c7c61cd2167f0dd74
ganeshdasrla <ganesh.dasarla@gmail.com>
2024-06-24 13:42:16 +0530

    more programs

--- CppProject/recursion/power1.cpp
+++ CppProject/recursion/power1.cpp
@@ -4,5 +4,19 @@ using namespace std;
 int pow(int x, int n) {
     if(n == 0) return 1;
     
-    if(n)
+    if(n%2  == 0) {
+        int half = pow(x, n / 2);
+        return half * half;
+
+    } else {
+        int half = pow(x, n / 2);
+        return x * half * half;
+    }
+}
+
+int main() {
+    int n, x;
+    cin>>x>>n;
+    cout<<pow(x, n);
+    return 0;
 }
